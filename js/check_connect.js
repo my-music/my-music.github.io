cf9eb661fd4c52ff68b2c39b376f158951d9f3d5
TranLeMinhB2014759 <minhb2014759@student.ctu.edu.vn>
2023-06-19 07:01:19 +0700

    Update

--- js/check_connect.js
+++ js/check_connect.js
@@ -1,14 +1,31 @@
-window.addEventListener('offline', function (){
-    document.getElementById('success').style.display = 'none';
-    setTimeout(function() {
-        document.getElementById('error').style.display = 'block';
-    }, 2000);
-})
+// window.addEventListener('offline', function (){
+//     document.getElementById('success').style.display = 'none';
+//     document.getElementById('error').style.display = 'block';
+// })
 
-window.addEventListener('online', function (){
-    document.getElementById('error').style.display = 'none';
-    document.getElementById('success').style.display = 'block';
-    setTimeout(function() {
-        document.getElementById('success').style.display = 'none';
-    }, 5000);
-});
+// window.addEventListener('online', function (){
+//     document.getElementById('error').style.display = 'none';
+//     document.getElementById('success').style.display = 'block';
+//     setTimeout(function() {
+//         document.getElementById('success').style.display = 'none';
+//     }, 5000);
+// });
+
+var success = document.getElementById('success');
+var error = document.getElementById('error');
+
+// initial status
+checkNetworkStatus();
+
+// check network status periodically
+setInterval(checkNetworkStatus, 3000);
+
+function checkNetworkStatus() {
+  if (navigator.onLine) {
+    error.style.display = 'none';
+    success.style.display = 'block';
+  } else {
+    success.style.display = 'none';
+    error.style.display = 'block';
+  }
+}
